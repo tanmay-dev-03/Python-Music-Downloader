184638b55853e3e1f5cd563bcb9608ed3c1d4e25
Tanmay Deshpande <tanmay.developer.18@gmail.com>
2020-12-11 17:44:05 +0530

    v1.0

---
+++ setup.py
@@ -0,0 +1,28 @@
+import sys
+import pkg_resources
+import subprocess as sp
+
+
+def setup():
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    required = {"rich", "questionary", "youtube-dl", "tabulate"}
+    missing = required - installed
+    if missing:
+        for module in missing:
+            try:
+                sp.check_call(
+                    [
+                        sys.executable,
+                        "-m",
+                        "pip",
+                        "install",
+                        "--no-cache-dir",
+                        module,
+                    ],
+                    stdout=sp.DEVNULL,
+                )
+            except sp.SubprocessError as e:
+                print(e)
+                exit()
+            else:
+                print("{} was installed successfully")
