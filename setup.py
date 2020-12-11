3ed28bc53c07829de0b4b1417311a761e9113b30
Tanmay Deshpande <tanmay.developer.18@gmail.com>
2020-12-11 17:47:08 +0530

    v1.1 - added documentation

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ import subprocess as sp
 
 
 def setup():
+    """Function for installing necessary modules i.e.:
+
+    - rich
+    - questionary
+    - youtube-dl
+    - tabulate"""
     installed = {pkg.key for pkg in pkg_resources.working_set}
     required = {"rich", "questionary", "youtube-dl", "tabulate"}
     missing = required - installed
