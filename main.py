b27571add585c7bd0e45c1e94371a564c682b965
Tanmay Deshpande <tanmay.developer.18@gmail.com>
2020-12-11 18:01:46 +0530

    added boilerplate for functions

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 from setup import setup
 
 setup()
+from rich import print
+import os
+import sys
+import questionary as qr
 
 
-# from rich import print
+class MainApp:
+    def __init__(self) -> None:
+        self.termSize = termSize = os.get_terminal_size()
+        print("-" * termSize.columns)
+        print("Music Downloader".center(termSize.columns))
+        print("-" * termSize.columns)
+
+    def chooseAction(self):
+        print("[b red] Choose An Action[/]".center(self.termSize.columns))
+        actions = [
+            "Download a PlayList",
+            "Download a Single Song",
+        ]
+
+        choice = qr.select(
+            "Choose an action:",
+            choices=actions,
+            default=actions[1],
+        ).ask()
+
+        if choice == actions[0]:
+            self.playListMode()
+        elif choice == actions[1]:
+            self.singleMode()
+
+    def playListMode(self):
+        print("[b green]PlayList mode[/]")
+
+    def singleMode(self):
+        print("[b green]Single Song Mode[/]")
