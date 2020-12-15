9b6758741f1c561ff3734a3627d97751d41115aa
Tanmay Deshpande <tanmay.developer.18@gmail.com>
2020-12-15 12:38:34 +0530

    fixed imports; started working on playlist mode

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from setup import setup
 
 setup()
-from rich import print
 import os
 import sys
+
 import questionary as qr
+import requests
+from rich import print
 
 
 class MainApp:
     def __init__(self) -> None:
         self.termSize = termSize = os.get_terminal_size()
+        print(sys.version)
+        print(sys.executable)
         print("-" * termSize.columns)
         print("Music Downloader".center(termSize.columns))
         print("-" * termSize.columns)
 
     def chooseAction(self):
-        print("[b red] Choose An Action[/]".center(self.termSize.columns))
+        print("[b red] Choose An Action[/]")
         actions = [
             "Download a PlayList",
             "Download a Single Song",
@@ -33,10 +37,31 @@ class MainApp:
             self.singleMode()
 
     def playListMode(self):
-        print("[b green]PlayList mode[/]")
+        """
+        Method for downloading songs from PlayList.
+        Uses youtube-dl under the hood.
+        """
+        print("[b blue]PlayList mode[/]")
+        while True:
+            url = qr.text(
+                "Enter the URL for the PlayList:", qmark="*", multiline=True
+            ).ask()
+
+            try:
+                requests.get(url)
+                # resp.status_code
+            except requests.ConnectionError as e:
+                print(e)
+                return
+            else:
+                print("URL validated successfully!")
 
     def singleMode(self):
-        print("[b green]Single Song Mode[/]")
+        """
+        Method for downloading a single song.
+        uses youtube-dl under the hood
+        """
+        print("[b blue]Single Song Mode[/]")
 
 
 if __name__ == "__main__":
